3363083fb430577835414c19853112bd8ef31b5e
Sergio <sergio.gomez2000@hotmail.com>
2025-01-30 15:55:29 -0500

    feat: docs complementation and search plugin

--- apps/docs/docusaurus.config.ts
+++ apps/docs/docusaurus.config.ts
@@ -21,6 +21,12 @@ const config: Config = {
   onBrokenLinks: "warn",
   onBrokenMarkdownLinks: "warn",
 
+  plugins: [
+    [
+      require.resolve("@cmfcmf/docusaurus-search-local"),
+      { language: "es", indexBlog: false },
+    ],
+  ],
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
   // may want to replace "en" with "zh-Hans".
@@ -68,6 +74,7 @@ const config: Config = {
         },
       ],
     },
+
     footer: {
       style: "dark",
       links: [
